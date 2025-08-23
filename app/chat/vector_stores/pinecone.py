57935ab42a417d8ca61f865b90300f2d4c779457
Rajavenkateshwaran <rajavn6789@gmail.com>
2025-08-23 20:08:29 +0530

    Pinecone config

---
+++ app/chat/vector_stores/pinecone.py
@@ -0,0 +1,22 @@
+from pinecone import Pinecone
+import os
+from langchain.vectorstores import Pinecone as LangchainPinecone
+from app.chat.embeddings.openai import embeddings  # your OpenAI embeddings
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+index_name = os.getenv("PINECONE_INDEX_NAME")
+
+
+if index_name not in [i["name"] for i in pc.list_indexes()]:
+    pc.create_index(
+        name=index_name,
+        dimension=1536,             
+        metric="cosine",
+        spec={"serverless": {"cloud": "aws", "region": "us-east-1"}}
+    )
+
+vector_store = LangchainPinecone.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
